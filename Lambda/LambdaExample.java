aa21132b4601af123adad2da89df3f041e2ca983
varunr10 <63897023+varunr10@users.noreply.github.com>
2020-07-25 16:58:45 +0530

    Added LambdaExample.java
    This example demonstrates a Lambda function using Java 8 runtime

---
+++ Lambda/LambdaExample.java
@@ -0,0 +1,18 @@
+package com.awslambda.LambdaUsingJava;
+
+/* Handler in lambda syntax -->  packageName.className::methodName  */
+
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+/* Class must implement RequestHandler interface and mention type of Input, Output. Object & Object in this case */
+public class LambdaExample implements RequestHandler<Object,Object>
+{
+  /* Handler method for the Lambda Function */
+	public Object handleRequest(Object input, Context context) {
+  
+		System.out.println("Welcome to Sample Lambda function using Java");
+		return null;
+	}
+}
